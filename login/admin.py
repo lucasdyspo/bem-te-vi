b2a15cb8387a847d0ae5de59e0f13c1e917f5f0f
lucasdyspo <80234431+lucasdyspo@users.noreply.github.com>
2022-05-05 16:51:59 -0300

    Add files via upload

---
+++ login/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Usuario
+
+
+# Register your models here.
+
+# @admin.register(Usuario())
+# class usuarioadmin(admin.ModelAdmin()):
+#     readonly_fields = ('nome', 'email', 'senha')
