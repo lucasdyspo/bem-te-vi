d866c2b06ad695f48295b7e24bc7afa0449ed345
lucasdyspo <80234431+lucasdyspo@users.noreply.github.com>
2022-05-05 17:01:04 -0300

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-
-# Register your models here.
-
-# @admin.register(Usuario())
-# class usuarioadmin(admin.ModelAdmin()):
-#     readonly_fields = ('nome', 'email', 'senha')
